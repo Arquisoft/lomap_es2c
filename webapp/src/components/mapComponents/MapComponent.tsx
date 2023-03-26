e42c2839ef35f0e553b0353c576f996cfee944d7
Paula Suarez Prieto - UO269745 <uo269745@uniovi.es>
2023-03-26 21:59:37 +0200

    clicking on the map shows a marker
    clicking on the map shows a marker so you can add a new place to your groups

--- webapp/src/components/mapComponents/MapComponent.tsx
+++ webapp/src/components/mapComponents/MapComponent.tsx
@@ -1,89 +1,80 @@
-import React, { useRef, useEffect, useState } from 'react';
+import {useState } from 'react';
 import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents } from 'react-leaflet'
 import L from 'leaflet';
-import { AddPlace } from './AddPlaceComponent';
-import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
 
 const MapBox = styled(Box)({
-    minWidth: "75vw",
-    backgroundColor: "brown"
+  minWidth: "75vw",
+  backgroundColor: "brown"
 })
+
+const center = {
+  lat: 50.8504500,
+  lng: 4.3487800
+}
+
+
+
+type MarkerData = {
+  position: [number, number];
+  name: string;
+};
+
+const markers: MarkerData[] = [
+  { position: [50.90, 4.4], name: 'M1' },
+  { position: [51, 4], name: 'M2' },
+  { position: [50.7, 4.4], name: 'M3' },
+];
+
+
+
+function AddPlace(): any {
+  const [marker, setMarker] = useState(null)
+  const map = useMap()
+
+  const m = useMapEvents({
+    click(e) {
+      if (marker !== null) {
+        marker.remove();
+      }
+
+      var newMarker: L.Marker = L.marker(e.latlng);
+
+      newMarker.bindPopup((new L.Popup({ keepInView: true })).setContent('<button id="addPlaceDyn">Añadir lugar</button>'))
+      setMarker(newMarker)
+      console.log(marker)
+
+    }
+  })
+
+  if (marker !== null) {
+    marker.addTo(map)
+  }
+
+  return null;
+}
+
+
 
 export const MapComponent = () => {
 
-    const [url, setUrl] = useState("../add-point.png");
-
-    var addIcon = L.icon({
-        iconUrl: url,
-    
-        iconSize:     [38, 55], // size of the icon
-        shadowSize:   [50, 64], // size of the shadow
-        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-        shadowAnchor: [4, 62],  // the same for the shadow
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-    });
-
-    function onMapClick(e:any) {
-        // AddPlaceComponent("prueba", "porfa", null)
-         addd(e.latlng[0], e.latlng[1])
-         //alert("You clicked the map at " + e.latlng);
-     }
- 
-     
-     function Prueba():any {
-         const map = useMap()
-         map.on('click', onMapClick);
-         
-         return null
-   }
- 
-   function addd(long:number, lat:number):any {
-     return <Marker position={[lat,long]} icon={addIcon}>
-                  <Popup keepInView={true}>
-                 <button>añadir lugar</button>
-                  </Popup>
-              </Marker>
- }
- 
- 
-   function Markers():any {
-        return <Marker position={[50.8504500, 4.3487800]} icon={addIcon}>
-                     <Popup keepInView={true}>
-                    <button>añadir lugar</button>
-                     </Popup>
-                 </Marker>
-   }
-
-    const mapRef = useRef<L.Map>(null);
-  
-    useEffect(() => {
-      if (mapRef.current) {
-        mapRef.current.setView([51.505, -0.09], 13);
-      }
-    }, []);
-  
-    return (
-      <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
-        style={{ height: '100vh', width: '100vw' }}
-        whenCreated={(map:any) => {
-           mapRef.current = map;
-        }}
-      >
-       <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
+  return (
+    <MapContainer center={center} zoom={13} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {markers.map(({ position, name }) => (
+        <Marker position={position} key={name}>
+          <Popup>{name}</Popup>
         </Marker>
-      </MapContainer>
-    );
-  };
-  
+      ))}
+      <AddPlace />
+    </MapContainer>
+
+  );
+
+};
+
 
