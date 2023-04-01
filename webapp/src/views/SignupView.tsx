2ddf0457c37ecba88bfbf71b843302e47f2ef609
uo269745 <71699020+uo269745@users.noreply.github.com>
2023-04-01 22:49:42 +0200

    first version login to the pod

--- webapp/src/views/SignupView.tsx
+++ webapp/src/views/SignupView.tsx
@@ -4,7 +4,8 @@ import { Paper } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { Header } from '../components/mainComponents/Header';
 import { Footer } from '../components/mainComponents/Footer';
-import { Signup } from '../components/userIdentification/signup';
+import { Signup } from '../components/userIdentification/Signup';
+import { PodLogin } from '../components/userIdentification/PodLogin2';
 
 //#region DEFINICION DE COMPONENTES STYLED
 const MyContainer = styled(Container)({
@@ -36,7 +37,7 @@ export default function SignupView() {
         //#region COMPONENTE
         <MyContainer disableGutters maxWidth={false}>
             <MyPaper elevation={1}><Header logged={false} /></MyPaper>
-            <Signup />
+            <PodLogin />
             <MyPaper2 elevation={1}><Footer /></MyPaper2>
         </MyContainer>
         //#endregion
