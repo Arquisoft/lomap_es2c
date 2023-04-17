52542226bc6c655bde6ab8fd4dbe209ee9b39697
uo282154 <adrianfernandezalvarez02@gmail.com>
2023-04-17 14:32:49 +0200

    Ahora ya funcionan las llamadas http a la restapi desde los tests

--- restapi/tests/api.test.ts
+++ restapi/tests/api.test.ts
@@ -1,24 +1,49 @@
+import express, {Application, RequestHandler} from "express";
+import bp from "body-parser";
+import promBundle from "express-prom-bundle";
+import { Server } from "http";
+import request, { Response } from "supertest";
+import apiUser from "../api";
+import mongoose from "mongoose";
 
-import supertest, { SuperTest, Test } from 'supertest';
-import app from '../server';
+var server: Server;
 
-
-let request: SuperTest<Test>;
+const app: Application = express();
 
 beforeAll(async () => {
+    const metricsMiddleware: RequestHandler = promBundle({ includeMethod: true });
+    app.use(metricsMiddleware);
 
-    request = supertest(app);
+    app.use(bp.json());
+
+    app.use(bp.urlencoded({ extended: false }));
+
+    app.use(apiUser);
+
+    server = app.listen(5000);
+
+});
+afterAll(async () => {
+    server.close();
+    await mongoose.connection.close();
 });
 
-describe('pruebaASecas ', () => {
-
+describe('test conexión con api ', () => {
     /**
      * Test that we can list users without any error.
      */
-    it('prueba1',async () => {
-        const response = await request.get('/test');
-        console.log(response+"   hola");
+     test('GET /',async () => {
+        const response = await request(app).get('/test');
         expect(response.status).toBe(200);
     });
+});
 
+describe('test api metodos usermanager ', () => {
+    /**
+     * Test that we can list users without any error.
+     */
+    test('GET /',async () => {
+        const response = await request(app).get('/test');
+        expect(response.status).toBe(200);
+    });
 });
