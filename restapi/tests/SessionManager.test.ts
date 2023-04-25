19ca00d0e226ba2e248f94bfa4867cbe5e35c98e
uo282154 <adrianfernandezalvarez02@gmail.com>
2023-04-25 17:14:48 +0200

    Añadido un test y cambiado nombre de archivos de test de SessionManager.test.ts y UserManager.test.ts

--- restapi/tests/SessionManager.test.ts
+++ restapi/tests/SessionManager.test.ts
@@ -1,15 +1,21 @@
 import {UserManager, UserManagerImpl} from '../src/controllers/UserManager';
 import { UserImpl } from '../src/entities/User';
 import User from "../src/entities/UserSchema";
+import {SesionManager} from "../src/facade";
+import {UserSesionManager} from "../src/controllers/SessionManager";
 import mongoose from "mongoose";
+import request from "supertest";
+import UserSchema from "../src/entities/UserSchema";
 
-describe('UserManager', () => {
+describe('SessionManager', () => {
     let userManager: UserManager;
+    let sessionManager: SesionManager;
 
     beforeEach(() => {
-        // Inicializar la instancia de UserManager antes de cada prueba
+        // Inicializar la instancia de UserManager y SessionManager antes de cada prueba
         require("../src/persistence/DataBase")
         userManager = new UserManagerImpl();
+        sessionManager = new UserSesionManager();
     });
 
     afterEach(() => {
@@ -20,115 +26,50 @@ describe('UserManager', () => {
         mongoose.connection.close()
     });
 
-    describe('listarDetalles', () => {
-        it('deberia devolver los detalles del usuario', async () => {
-            const usuario=await userManager.listarDetalles(new UserImpl("usertestn1", "", "", ""))
+    describe('iniciarSesion', () => {
+        it('deberia retornar el usuario si las credenciales son correctas', async () => {
+            const usuario=await sessionManager.iniciarSesion(new UserImpl("usertestn1", "usertestN1.", "", ""))
             expect(usuario.webID).toBe("webIdPrueba1");
-
         });
+        it('deberia lanzar un error si la contraseña es incorrecta', async () => {
 
-        it('debería devolver la contraseña vacia', async () => {
-            const usuario=await userManager.listarDetalles(new UserImpl("usertestn1", "", "", ""))
-            expect(usuario.password).toBe("");
-        });
-
-        it('debería lanzar un error si el usuario no existe', async () => {
             let error=new Error("Error por defecto")
             try{
-                await userManager.listarDetalles(new UserImpl("usertestnotexists", "", "", ""))
+                await sessionManager.iniciarSesion(new UserImpl("usertestn1", "contrasenaincorrecta", "", ""))
             }catch(err){
                 error=err;
             }
-            expect(error.message).toBe("El usuario no existe.");
+            expect(error.message).toBe("Contraseña incorrecta");
         });
-        it('deberia devolver la descripcion de un usuario con descripcion', async () => {
-            const usuario=await userManager.listarDetalles(new UserImpl("usertestn1", "", "", ""))
-            expect(usuario.description).toBe("descripcionprueba1");
-        });
-        it('deberia devolver los detalles de un usuario sin descripcion, descripcion=undefined', async () => {
-            const usuario=await userManager.listarDetalles(new UserImpl("usertestn2", "", "", ""))
-            expect(usuario.description).toBe(undefined);
+        it('deberia lanzar un error si el usuario no existe', async () => {
+
+            let error=new Error("Error por defecto")
+            try{
+                await sessionManager.iniciarSesion(new UserImpl("usernotexists", "contrasenaincorrecta", "", ""))
+            }catch(err){
+                error=err;
+            }
+            expect(error.message).toBe("Usuario no encontrado");
         });
     });
 
-    describe('modificarPerfil', () => {
-        it('debería modificar correctamente el perfil de un usuario existente', async () => {
-            //se modifica
-            const usuario=await userManager.modificarPerfil(new UserImpl("usertestn3", "", "webid cambiado", ""))
-            expect(usuario.webID).toBe("webid cambiado");
-            //se comprueba
-            const usuario2=await userManager.listarDetalles(new UserImpl("usertestn3", "", "", ""))
-            expect(usuario2.webID).toBe("webid cambiado");
-            //se deja como estaba para poder volver a ejecutar la prueba
-            const usuario3=await userManager.modificarPerfil(new UserImpl("usertestn3", "", "webIdPrueba3", ""))
-            expect(usuario3.webID).toBe("webIdPrueba3");
-            //se vuelve a comprobar
-            const usuario4=await userManager.listarDetalles(new UserImpl("usertestn3", "", "", ""))
-            expect(usuario4.webID).toBe("webIdPrueba3");
 
-        });
-
-        it('debería lanzar un error si el usuario no existe', async () => {
+    describe('registrarse', () => {
+        it('deberia lanzar un error si el usuario ya existe', async () => {
             let error=new Error("Error por defecto")
             try{
-                await userManager.modificarPerfil(new UserImpl("usertestnotexists", "", "", ""))
+                await sessionManager.registrarse(new UserImpl("usertestn1", "", "", ""))
             }catch(err){
                 error=err;
             }
-            expect(error.message).toBe("Usuario no encontrado.");
+            expect(error.message).toBe("Usuario ya existente");
         });
-        it('debería devolver la contraseña vacia', async () => {
-            //se modifica
-            const usuario=await userManager.modificarPerfil(new UserImpl("usertestn3", "", "webid cambiado", ""))
-            expect(usuario.password).toBe("");
-
-            //se deja como estaba para poder volver a ejecutar la prueba
-            const usuario2=await userManager.modificarPerfil(new UserImpl("usertestn3", "", "webIdPrueba3", ""))
-            expect(usuario2.password).toBe("");
-
-
+        it('deberia registrarase correctamente', async () => {
+            let user=new UserImpl("usertestsignup","1234","webIdPrueba","descripcionprueba","img")
+            await sessionManager.registrarse(user)
+            let result=await userManager.listarDetalles(user)
+            expect(result.username).toBe(user.username);
+            await UserSchema.deleteOne({username:"usertestsignup"})
         });
-
-
-
     });
-    describe('modificarContrasena', () => {
-        it('debería lanzar un error si el usuario no existe', async () => {
-            let error=new Error("Error por defecto")
-            try{
-                await userManager.modificarContrasena(new UserImpl("usertestnotexists", "", "", ""),"","")
-            }catch(err){
-                error=err;
-            }
-            expect(error.message).toBe("El usuario no se encuentra");
-
-        });
-        it('debería lanzar un error si la contraseña antigua no coincide', async () => {
-            let error=new Error("Error por defecto")
-            try{
-                await userManager.modificarContrasena(new UserImpl("usertestn4", "", "", ""),"contrasenaincorrecta","usertestN4.")
-            }catch(err){
-                error=err;
-            }
-            expect(error.message).toBe("La contraseña antigua era incorrecta");
-
-        });
-
-        it('no deberia lanzar ningun error si cambia la contraseña satisfactoriamente (se cambia por la misma por ser un test)', async () => {
-            let error=new Error("Error por defecto")
-            try{
-                await userManager.modificarContrasena(new UserImpl("usertestn4", "", "", ""),"usertestN4.","usertestN4.")
-            }catch(err){
-                expect(false).toBe(true);
-            }
-            expect(true).toBe(true);
-
-        });
-
-
-
-
-
-    });
-
 });
--- restapi/tests/UserManager.test.ts
+++ restapi/tests/UserManager.test.ts
@@ -1,19 +1,15 @@
 import {UserManager, UserManagerImpl} from '../src/controllers/UserManager';
 import { UserImpl } from '../src/entities/User';
 import User from "../src/entities/UserSchema";
-import {SesionManager} from "../src/facade";
-import {UserSesionManager} from "../src/controllers/SessionManager";
 import mongoose from "mongoose";
 
-describe('SessionManager', () => {
+describe('UserManager', () => {
     let userManager: UserManager;
-    let sessionManager: SesionManager;
 
     beforeEach(() => {
-        // Inicializar la instancia de UserManager y SessionManager antes de cada prueba
+        // Inicializar la instancia de UserManager antes de cada prueba
         require("../src/persistence/DataBase")
         userManager = new UserManagerImpl();
-        sessionManager = new UserSesionManager();
     });
 
     afterEach(() => {
@@ -24,43 +20,115 @@ describe('SessionManager', () => {
         mongoose.connection.close()
     });
 
-    describe('iniciarSesion', () => {
-        it('deberia retornar el usuario si las credenciales son correctas', async () => {
-            const usuario=await sessionManager.iniciarSesion(new UserImpl("usertestn1", "usertestN1.", "", ""))
+    describe('listarDetalles', () => {
+        it('deberia devolver los detalles del usuario', async () => {
+            const usuario=await userManager.listarDetalles(new UserImpl("usertestn1", "", "", ""))
             expect(usuario.webID).toBe("webIdPrueba1");
-        });
-        it('deberia lanzar un error si la contraseña es incorrecta', async () => {
 
+        });
+
+        it('debería devolver la contraseña vacia', async () => {
+            const usuario=await userManager.listarDetalles(new UserImpl("usertestn1", "", "", ""))
+            expect(usuario.password).toBe("");
+        });
+
+        it('debería lanzar un error si el usuario no existe', async () => {
             let error=new Error("Error por defecto")
             try{
-                await sessionManager.iniciarSesion(new UserImpl("usertestn1", "contrasenaincorrecta", "", ""))
+                await userManager.listarDetalles(new UserImpl("usertestnotexists", "", "", ""))
             }catch(err){
                 error=err;
             }
-            expect(error.message).toBe("Contraseña incorrecta");
+            expect(error.message).toBe("El usuario no existe.");
         });
-        it('deberia lanzar un error si el usuario no existe', async () => {
-
-            let error=new Error("Error por defecto")
-            try{
-                await sessionManager.iniciarSesion(new UserImpl("usernotexists", "contrasenaincorrecta", "", ""))
-            }catch(err){
-                error=err;
-            }
-            expect(error.message).toBe("Usuario no encontrado");
+        it('deberia devolver la descripcion de un usuario con descripcion', async () => {
+            const usuario=await userManager.listarDetalles(new UserImpl("usertestn1", "", "", ""))
+            expect(usuario.description).toBe("descripcionprueba1");
+        });
+        it('deberia devolver los detalles de un usuario sin descripcion, descripcion=undefined', async () => {
+            const usuario=await userManager.listarDetalles(new UserImpl("usertestn2", "", "", ""))
+            expect(usuario.description).toBe(undefined);
         });
     });
 
+    describe('modificarPerfil', () => {
+        it('debería modificar correctamente el perfil de un usuario existente', async () => {
+            //se modifica
+            const usuario=await userManager.modificarPerfil(new UserImpl("usertestn3", "", "webid cambiado", ""))
+            expect(usuario.webID).toBe("webid cambiado");
+            //se comprueba
+            const usuario2=await userManager.listarDetalles(new UserImpl("usertestn3", "", "", ""))
+            expect(usuario2.webID).toBe("webid cambiado");
+            //se deja como estaba para poder volver a ejecutar la prueba
+            const usuario3=await userManager.modificarPerfil(new UserImpl("usertestn3", "", "webIdPrueba3", ""))
+            expect(usuario3.webID).toBe("webIdPrueba3");
+            //se vuelve a comprobar
+            const usuario4=await userManager.listarDetalles(new UserImpl("usertestn3", "", "", ""))
+            expect(usuario4.webID).toBe("webIdPrueba3");
 
-    describe('registrarse', () => {
-        it('deberia lanzar un error si el usuario ya existe', async () => {
+        });
+
+        it('debería lanzar un error si el usuario no existe', async () => {
             let error=new Error("Error por defecto")
             try{
-                await sessionManager.registrarse(new UserImpl("usertestn1", "", "", ""))
+                await userManager.modificarPerfil(new UserImpl("usertestnotexists", "", "", ""))
             }catch(err){
                 error=err;
             }
-            expect(error.message).toBe("Usuario ya existente");
+            expect(error.message).toBe("Usuario no encontrado.");
         });
+        it('debería devolver la contraseña vacia', async () => {
+            //se modifica
+            const usuario=await userManager.modificarPerfil(new UserImpl("usertestn3", "", "webid cambiado", ""))
+            expect(usuario.password).toBe("");
+
+            //se deja como estaba para poder volver a ejecutar la prueba
+            const usuario2=await userManager.modificarPerfil(new UserImpl("usertestn3", "", "webIdPrueba3", ""))
+            expect(usuario2.password).toBe("");
+
+
+        });
+
+
+
     });
+    describe('modificarContrasena', () => {
+        it('debería lanzar un error si el usuario no existe', async () => {
+            let error=new Error("Error por defecto")
+            try{
+                await userManager.modificarContrasena(new UserImpl("usertestnotexists", "", "", ""),"","")
+            }catch(err){
+                error=err;
+            }
+            expect(error.message).toBe("El usuario no se encuentra");
+
+        });
+        it('debería lanzar un error si la contraseña antigua no coincide', async () => {
+            let error=new Error("Error por defecto")
+            try{
+                await userManager.modificarContrasena(new UserImpl("usertestn4", "", "", ""),"contrasenaincorrecta","usertestN4.")
+            }catch(err){
+                error=err;
+            }
+            expect(error.message).toBe("La contraseña antigua era incorrecta");
+
+        });
+
+        it('no deberia lanzar ningun error si cambia la contraseña satisfactoriamente (se cambia por la misma por ser un test)', async () => {
+            let error=new Error("Error por defecto")
+            try{
+                await userManager.modificarContrasena(new UserImpl("usertestn4", "", "", ""),"usertestN4.","usertestN4.")
+            }catch(err){
+                expect(false).toBe(true);
+            }
+            expect(true).toBe(true);
+
+        });
+
+
+
+
+
+    });
+
 });
