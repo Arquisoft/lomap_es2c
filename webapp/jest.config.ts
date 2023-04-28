7067bd81bd7725a5b9705a5eec319bf73b5ab41f
Abel Busto Dopazo <UO284262@uniovi.es>
2023-04-28 13:46:25 +0200

    Test jest.config.ts

--- webapp/jest.config.ts
+++ webapp/jest.config.ts
@@ -1,16 +1,11 @@
 export default {
+    rootDir: './',
     transform: {
         "^.+\\.tsx?$": "ts-jest"
     },
     collectCoverage: true,
     collectCoverageFrom: [
-        "src/auth/**.{ts,tsx}",
-        "!server.ts",
-        "!src/api/**.{ts,tsx}",
-        "src/components/**.{ts,tsx}",
-        "!src/views/**.{ts,tsx}",
-        "!src/utils/**.{ts,tsx}",
-        "!src/podManager/**.{ts,tsx}",
-        "!src/shared/**.{ts,tsx}",
+        "src/auth/**.tsx",
+        "src/components/**.tsx",
     ]
 }
