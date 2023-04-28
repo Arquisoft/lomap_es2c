3707d1ad75a1b000c4ea9d2d02169825e7a046ac
Abel Busto Dopazo <UO284262@uniovi.es>
2023-04-28 14:23:54 +0200

    jest.config.ts test

--- webapp/jest.config.ts
+++ webapp/jest.config.ts
@@ -1,11 +1,12 @@
 export default {
-    rootDir: './',
     transform: {
         "^.+\\.tsx?$": "ts-jest"
     },
-    collectCoverage: true,
-    collectCoverageFrom: [
-        "src/auth/**.tsx",
-        "src/components/**.tsx",
-    ]
 }
+
+/*"jest": {
+        "collectCoverageFrom": [
+            "src/auth/**.tsx",
+            "src/components/**.tsx"
+        ]
+    },*/
