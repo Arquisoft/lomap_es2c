23eeec3c2a1c4415199e0a0cc09c6da6205996b4
uo269745 <paulasp4esobarredos@gmail.com>
2023-04-29 15:41:24 +0200

    hot fix

--- restapi/src/persistence/DataBase.tsx
+++ restapi/src/persistence/DataBase.tsx
@@ -3,6 +3,6 @@ require("dotenv").config();
 
 const uri:string = process.env.DATABASE_URL;
 
-mongoose.connect(uri!).then(() =>{
+mongoose.connect(uri).then(() =>{
   console.log('Database succesfully connected!');
 });
