4002dbc175ad82fb2737236bdbacc0d27aaad29a
uo269745 <paulasp4esobarredos@gmail.com>
2023-04-30 21:17:29 +0200

    another try to run e2e

--- webapp/server.ts
+++ webapp/server.ts
@@ -11,10 +11,7 @@ var app: Application = express();
 const HTTPS_PORT = 443;
 const HTTP_PORT = 3000;
 
-const options = {
-  cert: fs.readFileSync('/etc/ssl/certs/lomapes2c.eastus.cloudapp.azure.com.crt'),
-  key: fs.readFileSync('/etc/ssl/private/lomapes2c.eastus.cloudapp.azure.com.key'),
-};
+
 
 app.use(express.static("build"));
 
@@ -35,18 +32,24 @@ app.use((req, res, next) => {
 });
 
 
-/*if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production")
+{
+  const options = {
+    cert: fs.readFileSync('/etc/ssl/certs/lomapes2c.eastus.cloudapp.azure.com.crt'),
+    key: fs.readFileSync('/etc/ssl/private/lomapes2c.eastus.cloudapp.azure.com.key'),
+  };
+  
   https.createServer(options, app).listen(HTTPS_PORT, () => {
     console.log(`Webapp started on port ${HTTPS_PORT}`);
   }).on("error", (error: Error) => {
       console.error("Error occured: " + error.message);
     });
 
-} else {*/
-  app.listen(HTTP_PORT, () => {
-    console.log(`Webapp started on port ${HTTP_PORT} (development)`);
-  }).on("error", (error: Error) => {
-    console.error("Error occured: " + error.message);
-  });
-//}
+} else {
+    app.listen(HTTP_PORT, () => {
+      console.log(`Webapp started on port ${HTTP_PORT} (development)`);
+    }).on("error", (error: Error) => {
+      console.error("Error occured: " + error.message);
+    });
+}
   
