efabb18608e5e54c10fe59a591b297f30bc3d8d9
Abel Busto Dopazo <UO284262@uniovi.es>
2023-05-01 13:20:32 +0200

    Some minor changes

--- restapi/src/persistence/DataBase.tsx
+++ restapi/src/persistence/DataBase.tsx
@@ -1,14 +1,14 @@
-var mongoose = require('mongoose');
+let mongoose = require('mongoose');
 require("dotenv").config();
 
 const uri = process.env.DATABASE_URL;
 
 mongoose.connect(uri,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).then(() =>{
-  console.log('Database succesfully connected!');
-}).catch((error: any) => {
-   console.log('Error connecting to database:', error);
-});
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log('Database succesfully connected!');
+    }).catch((error: any) => {
+        console.log('Error connecting to database:', error);
+    });
